99c9eb10a43d1f7e62421468dbac830670cbfdaa
Arvids Gargurnis <arvids.gargurnis@plymouth.ac.uk>
2018-04-11 11:17:08 +0100

    Learned how to use cocoa pods and implement sign in feature using social website accounts

--- socialLogin/AppDelegate.swift
+++ socialLogin/AppDelegate.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        GIDSignIn.sharedInstance().handle(url, sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String, annotation: [:])
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        
+        return handled
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let err = error {
+            print(err)
+            return
+        }
+        
+        guard let idToken = user.authentication.idToken else { return }
+        guard let accessToken = user.authentication.accessToken else { return }
+        let credentials = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+        Auth.auth().signIn(with: credentials) { (user, error) in
+            if let err = error {
+                print(err)
+            }
+            
+            guard let uid = user?.uid else { return }
+            print("success", uid)
+        }
+        print("success", user)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
--- socialLogin/ViewController.swift
+++ socialLogin/ViewController.swift
@@ -7,19 +7,112 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupFBButtons()
+        setupGoogleButtons()
+        
+    }
+    
+    @objc func handleCustomFBLogin() {
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            self.showEmailAddress()
+        }
+    }
+    
+    @objc func handleCustomGoogleLogin() {
+        GIDSignIn.sharedInstance().signIn()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        // succsefully loged in
+        print("success1")
+        showEmailAddress()
     }
-
+    
+    func setupGoogleButtons() {
+        // add google sign in button
+        let googleButton = GIDSignInButton()
+        view.addSubview(googleButton)
+        googleButton.frame = CGRect(x: 16, y: 182, width: view.frame.width - 32, height: 50)
+        
+        // add custom button
+        let customButton = UIButton(type: .system)
+        customButton.frame = CGRect(x: 16, y: 248, width: view.frame.width - 32, height: 50)
+        customButton.backgroundColor = .red
+        customButton.setTitle("Custom Google Sign in", for: .normal)
+        customButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        customButton.setTitleColor(.white, for: .normal)
+        view.addSubview(customButton)
+        customButton.addTarget(self, action: #selector(handleCustomGoogleLogin), for: .touchUpInside)
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+    
+    func setupFBButtons() {
+        // facebook sign in button
+        let loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = ["email"]
+        view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        loginButton.delegate = self
+        
+        //add custom fb buton
+        let customFBButton = UIButton(type: .system)
+        customFBButton.backgroundColor = .blue
+        customFBButton.setTitleColor(.white, for: .normal)
+        customFBButton.frame = CGRect(x: 16, y: 116, width: view.frame.width - 32, height: 50)
+        customFBButton.setTitle("Custom FB Login", for: .normal)
+        customFBButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        view.addSubview(customFBButton)
+        customFBButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
+    }
+    
+    func showEmailAddress() {
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else { return }
+        
+        let credentials = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        Auth.auth().signIn(with: credentials) { (user, error) in
+            if error != nil {
+                print(error ?? "")
+                return
+            }
+            
+            print("successs", user ?? "")
+        }
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields" :"id, name,email"]).start {
+            (connection, result, err) in
+            
+            if err != nil {
+                print(err!)
+                return
+            }
+            
+            print(result!)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        //did log out of facebook
+        print("logged out")
+    }
 
 }
 
